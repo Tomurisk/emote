7a9d43bdbf04f852ab36c01dac37584d73f76230
Tomurisk <54245284+Tomurisk@users.noreply.github.com>
2026-03-20 23:16:02 +0200

    Pinned appimagetool 1.9.1 SHA256 and signature checking for type2-runtime

--- build.sh
+++ build.sh
@@ -8,28 +8,29 @@ alias wget='wget --https-only --secure-protocol=TLSv1_2'
 
 VERSION="4.1.0"
 APPDIR="$(pwd)/AppDir"
-PATCH_URL="https://patch-diff.githubusercontent.com/raw/tom-james-watson/Emote/pull/154.patch"
+AIT_DIR="/tmp/appimagetool"
+AIT_VER="1.9.1"
 PYVER="3.6"
 
 TARBALL_MD5="80df80daadbc81bbae639d6f0dd3d4e8"
 PATCH_MD5="46e93b3acd34dac8aa0c77b16f362bad"
 OPENMOJI_MD5="8410259d3ba38859a65e8a2c8c4f6687"
+AIT_SHA256="ed4ce84f0d9caff66f50bcca6ff6f35aae54ce8135408b3fa33abfc3cb384eb0"
+
+# Clear old resources
+rm -rf "$APPDIR" "$AIT_DIR" Emote-* "v${VERSION}.tar.gz"
 
 ###############################################
 # Fetch appimagetool dynamically
 ###############################################
 
-APPIMAGETOOL="$HOME/Programs/appimagetool-x86_64.AppImage"
-AIT_SHA256=$(wget -qO- https://api.github.com/repos/AppImage/appimagetool/releases/latest \
-  | jq -r '.assets[] | select(.name=="appimagetool-x86_64.AppImage") | .digest' \
-  | cut -d: -f2)
-
-mkdir -p "$HOME/Programs"
+APPIMAGETOOL="$AIT_DIR/appimagetool-x86_64.AppImage"
+mkdir -p "$AIT_DIR"
 
 if [ ! -f "$APPIMAGETOOL" ]; then
     echo "Downloading appimagetool..."
     wget -O "$APPIMAGETOOL" \
-      "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
+      "https://github.com/AppImage/appimagetool/releases/download/${AIT_VER}/appimagetool-x86_64.AppImage"
 
     if echo "$AIT_SHA256  $APPIMAGETOOL" | sha256sum -c -; then
         echo "appimagetool checksum OK"
@@ -50,13 +51,12 @@ python${PYVER} -m pip install --user --upgrade pip setproctitle
 # Prepare sources
 ###############################################
 
-rm -rf "$APPDIR" Emote-* "v${VERSION}.tar.gz"
-
 wget -O "v${VERSION}.tar.gz" \
   "https://github.com/tom-james-watson/Emote/archive/refs/tags/v${VERSION}.tar.gz"
 
 # Removes manimpango
-wget -O 154.patch "$PATCH_URL"
+wget -O 154.patch \
+  "https://patch-diff.githubusercontent.com/raw/tom-james-watson/Emote/pull/154.patch"
 
 if echo "$TARBALL_MD5  v${VERSION}.tar.gz" | md5sum -c - \
    && echo "$PATCH_MD5  154.patch" | md5sum -c -; then
@@ -575,13 +575,43 @@ chmod +x "$APPDIR/AppRun"
 # Build AppImage
 ###############################################
 
-ARCH=x86_64 "$APPIMAGETOOL" --appimage-extract-and-run "$APPDIR"
+RUNTIME="runtime-x86_64"
+
+gpg --import <<'EOF'
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mDMEZjaeexYJKwYBBAHaRw8BAQdAhvHdHoBweX0uVRgfcnlzexrSg+TAbK2mU1TA
+gi0TMC20NEFwcEltYWdlIHR5cGUgMiBydW50aW1lIDx0eXBlMi1ydW50aW1lQGFw
+cGltYWdlLm9yZz6IlgQTFggAPgIbAwULCQgHAgYVCgkICwIEFgIDAQIeAQIXgBYh
+BFcMd6zqQMDxt1iQLL+WzKVkkPaVBQJmN7FgBQkSzRXlAAoJEL+WzKVkkPaVCXsA
+/0JxQPlr2AlKalt9LAGCXU633gBoXh8/sQQngGGWjhT2APoCls0XWL2qhx1jAIdr
+AqDmOi3bdzBOpWBBIsOexhbdBrg4BGY2nnsSCisGAQQBl1UBBQEBB0CRVIEEu+Ft
+W68O33iZCVDMIYUWdD59iXfQ7rHf8HxAEgMBCAeIfgQYFggAJhYhBFcMd6zqQMDx
+t1iQLL+WzKVkkPaVBQJmNp57AhsMBQkDwmcAAAoJEL+WzKVkkPaVY7oA/icTs/E6
+47LTon7ua021HdjQlwkHZOpa/hqBWQEB3w6GAQCbaPRxKcNN9Yfwxc6cIvfUORKz
++4OQzyesHV5P4fYLDw==
+=r/5H
+-----END PGP PUBLIC KEY BLOCK-----
+EOF
+
+wget -O "$AIT_DIR/runtime-x86_64.sig" \
+  "https://github.com/AppImage/type2-runtime/releases/download/continuous/$RUNTIME.sig"
+wget -O "$AIT_DIR/runtime-x86_64" \
+  "https://github.com/AppImage/type2-runtime/releases/download/continuous/$RUNTIME"
+
+if gpg --verify "$AIT_DIR/$RUNTIME.sig" "$AIT_DIR/$RUNTIME" 2>/dev/null; then
+    echo "Runtime signature OK"
+    ARCH=x86_64 "$APPIMAGETOOL" --appimage-extract-and-run --no-appstream --runtime-file "$AIT_DIR/$RUNTIME" "$APPDIR"
+else
+    echo "ERROR: Signature verification failed!"
+    exit 1
+fi
 
 ###############################################
 # Cleanup
 ###############################################
 
 shopt -s extglob
-rm -rf "$APPDIR" Emote-!(*.AppImage) "v${VERSION}.tar.gz"
+rm -rf "$APPDIR" "$AIT_DIR" Emote-!(*.AppImage) "v${VERSION}.tar.gz"
 
 echo "Done"
